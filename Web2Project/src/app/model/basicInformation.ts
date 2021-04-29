fe3e6777af14cadd69d4bec7b13268a3e9ef931f
dejan.subasic <dejan.subasic@gmail.com>
2021-04-29 16:47:45 +0200

    defined basic information

--- Web2Project/src/app/model/basicInformation.ts
+++ Web2Project/src/app/model/basicInformation.ts
@@ -1,3 +1,26 @@
 export interface BasicInformation{
-    
+    id: string;
+    tip: IncidentType;
+    priority: number;
+    confirmed: boolean;
+    status: IncidentStatus;
+    ETA: Date;    
+    ATA : Date; //actual time of arrival
+    outageTime: Date;
+    ETR : Date; //estimated time to restore
+    affectedCustomers: number;
+    calls: number;
+    voltage: number; //kV
+    scheduledTime: Date;
+    selfAssign: boolean; 
+}
+
+enum IncidentType {
+    Unplanned = 0,
+    Planned = 1
+}
+
+enum IncidentStatus {
+    Dispatched = 0,
+    //todo ???
 }
