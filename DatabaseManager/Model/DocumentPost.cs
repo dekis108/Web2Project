085eb9e52d58e0cab04ed7e24ad7e8a1c4cb41b1
dejan.subasic <dejan.subasic@gmail.com>
2021-06-17 22:12:53 +0200

    AddDocument

---
+++ DatabaseManager/Model/DocumentPost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Model
+{
+    public class DocumentPost
+    {
+        /*
+         export interface DocumentPost {
+    planned: boolean,
+    datetime: string,
+    details: string,
+    notes: string,
+    phoneNumber: string,
+    createdBy: string,
+
+    workOperationsCompleted: boolean,
+    tagsRemoved: boolean,
+    groundingRemoved: boolean,
+    ready: boolean
+    }*/
+
+        public bool planned { get; set; }
+
+        public string datetime { get; set; }
+
+        public string details { get; set; }
+
+        public string notes { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string createdBy { get; set; }
+
+        public bool workOperationsCompleted { get; set; }
+        public bool tagsRemoved { get; set; }
+
+        public bool groundingRemoved { get; set; }
+        public bool ready { get; set; }
+    }
+}
--- ServiceApp/Controllers/SecurityDocumentController.cs
+++ ServiceApp/Controllers/SecurityDocumentController.cs
@@ -45,5 +45,47 @@ namespace ServiceApp.Controllers
             DocumentWidgetInfo incidentWidgetInfo = new DocumentWidgetInfo(drafts, cancelled, executing, issued);
             return Ok(incidentWidgetInfo);
         }
+
+        [HttpPut]
+        [Route("AddDocument/{planned}/{datetime}/{details}/{notes}/{phoneNumber}/{createdBy}/{workOperationsCompleted}/{tagsRemoved}/{groundingRemoved}/{ready}")]
+        public async Task<IActionResult> AddDocument(bool planned, string datetime, string details, string notes,
+            string phoneNumber, string createdBy, bool workOperationsCompleted, bool tagsRemoved, bool groundingRemoved, bool ready)
+        {
+            /*
+            int count = _context.PlanoviRadaTB.Count() + 1;
+            string plID = count.ToString();
+            PlanRada pl = new PlanRada(plID, userid, createdby, DateTime.Parse(datecreated), company, tipnacemu, DateTime.Parse(startdate), DateTime.Parse(enddate), tiprada, phoneno, "", "", "", adresa, svrha, detalji, beleske);
+            pl.Status = status;
+            pl.UserID = userid;
+            _context.Add(pl);
+            await _context.SaveChangesAsync();
+            */
+
+            DocumentInfo info = new DocumentInfo();
+            SecurityDocument newDoc = new SecurityDocument();
+            try
+            {
+                string DId = "SD_" + datetime;
+
+
+                info.Id = "Info_" + DId;
+                info.Creator = _context.UserDatas.Find(createdBy);
+                info.Date = DateTime.Parse(datetime);
+                info.Status = DocumentStatus.Draft;
+                info.Details = details;
+                info.Notes = notes;
+                info.PhoneNumber = phoneNumber;
+                info.Planned = planned;
+
+                newDoc.Id = DId;
+                newDoc.DocumentInfo = info;
+            }
+            catch{
+
+            }
+
+
+            return Ok();
+        }
     }
 }
