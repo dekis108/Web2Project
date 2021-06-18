551be23637a53375fa4b1db1648aad1b91009530
dejan.subasic <dejan.subasic@gmail.com>
2021-06-18 19:15:10 +0200

    AddDevice

--- ServiceApp/Controllers/DeviceController.cs
+++ ServiceApp/Controllers/DeviceController.cs
@@ -28,5 +28,25 @@ namespace ServiceApp.Controllers
         {
             return Ok(_context.Devices);
         }
+
+        [HttpPut]
+        [Route("AddDevice/{name}/{address}/{coordinates}/{type}/{priority}")]
+        public async Task<IActionResult> AddDevice(string name, string address, string coordinates, string type, int priority)
+        {
+            int count = _context.Devices.Count() + 1;
+            Device device = new Device()
+            {
+                Id = "Dev_" + count,
+                Address = address,
+                Coords = coordinates,
+                Name = name,
+                Priority = priority,
+                Type = (DeviceType)Enum.Parse(typeof(DeviceType), type),
+            };
+
+            _context.Devices.Add(device);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
